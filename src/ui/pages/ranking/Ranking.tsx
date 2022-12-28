96b11e2386c433cfc88e5e95d91c503ccd603cbc
Gabriel Borges <gabriel.carvalho@terramagna.com.br>
2022-12-28 08:31:11 -0300

    feat: redirect user when accessing ranking page directly

--- src/ui/pages/ranking/Ranking.tsx
+++ src/ui/pages/ranking/Ranking.tsx
@@ -1,6 +1,12 @@
 import { PageProps } from "@/ui/pages/Router";
+import { useEffect } from "react";
 
 type Props = PageProps;
 export function Ranking({ page }: Props) {
+  useEffect(() => {
+    if (page.data == undefined) {
+      page.goToPage("/");
+    }
+  }, []);
   return <> hello fresh </>;
 }
--- tests/ui/pages/ranking/Ranking.spec.tsx
+++ tests/ui/pages/ranking/Ranking.spec.tsx
@@ -1,5 +1,22 @@
+import { render } from "@testing-library/react";
+import { Ranking } from "@/ui/pages/ranking/Ranking";
+
 describe("Ranking", () => {
-  test("test foo ", () => {
-    expect(1).toBe(2);
-  });
+  test.each([[undefined], [null]])(
+    "redirect user to a list page when page.data is '%s'",
+    (data) => {
+      const goToPage = jest.fn();
+      render(
+        <Ranking
+          page={{
+            goToPage,
+            data,
+          }}
+        />
+      );
+
+      expect(goToPage).toBeCalledTimes(1);
+      expect(goToPage).toBeCalledWith("/");
+    }
+  );
 });
